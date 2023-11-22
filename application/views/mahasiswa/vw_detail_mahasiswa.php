97587380566a19df8fe18e6c825faa9da471833b
yudhafebriansyahh <yudhafby1314@gmail.com>
2023-11-22 10:07:05 +0700

    Update vw_detail_mahasiswa.php

--- application/views/mahasiswa/vw_detail_mahasiswa.php
+++ application/views/mahasiswa/vw_detail_mahasiswa.php
@@ -15,7 +15,7 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="card-header">
-                                    Detail mahasiswa pcr
+                                    Detail mahasiswa
                                 </div>
                                 <div class="card-body">
                                     <h2 class="card-title"><?= $mahasiswa['nama']; ?></h2>
